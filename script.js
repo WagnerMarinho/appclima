1413925e10d5aa9308075d58518411d7cc596ea8
Wagner Marinho <wagnermarinho145@gmail.com>
2025-07-23 21:27:00 -0300

    Inicialização de projeto

---
+++ script.js
@@ -0,0 +1,15 @@
+elementos = {
+    pesquisaInput: document.querySelector(".pesquisa-input"),
+    pesquisaBtn: document.querySelector(".btn-pesquisa"),
+    nomeCidade: document.querySelector(".LagoaDeDentro"),
+    temperatura: document.querySelector(".temperatura"),
+    descricao: document.querySelector(".descrição"),
+    umidade: document.querySelector(".value-umidade"),
+    vento: document.querySelector(".value-vento"),
+    chuva: document.querySelector(".value-chuva"),
+    previsao: document.querySelector(".previsao"),
+    loading: document.querySelector(".load"),
+    erro: document.querySelector(".erro"),
+    climaDisplay: document.querySelector(".display-clima")
+}
+
